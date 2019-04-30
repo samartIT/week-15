039ac13ed2f26a09d95701222d9c860a8e4e65e0
samartIT <41602825+samartIT@users.noreply.github.com>
2019-04-30 19:19:14 +0700

    complete game in my branch

---
+++ week15/Assets/Scripts/Devices/DeviceTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceTrigger : MonoBehaviour {
+	[SerializeField] private GameObject[] targets;
+
+	public bool requireKey;
+
+	void OnTriggerEnter(Collider other){
+		if (requireKey && Managers.Inventory.equippedItem != "key")
+			return;
+			
+		foreach (GameObject target in targets) {
+			target.SendMessage ("Activate"); // calls "Activate" method
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		foreach (GameObject target in targets) {
+			target.SendMessage ("Deactivate"); // calls "Activate" method
+		}
+	}
+}
---
+++ week15/Assets/Scripts/UIController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Text healthLabel;
+    [SerializeField] private InventoryPopup popup;
+    [SerializeField] private Text levelEnding;
+
+    public void SaveGame()
+    {
+        Managers.Data.SaveGameState();
+    }
+
+    public void LoadGame()
+    {
+        Managers.Data.LoadGameState();
+    }
+
+    private void Awake()
+    {
+        Messenger.AddListener(GameEvent.HEALTH_UPDATED, OnHealthUpdated);
+        Messenger.AddListener(GameEvent.LEVEL_COMPLETE, OnLevelComplete);
+        Messenger.AddListener(GameEvent.LEVEL_FAILED, OnLevelFailed);
+        Messenger.AddListener(GameEvent.GAME_COMPLETE, OnGameComplete);
+    }
+
+    public void OnGameComplete()
+    {
+        levelEnding.gameObject.SetActive(true);
+        levelEnding.text = "You have completed game";
+    }
+
+    private void OnDestroy()
+    {
+        Messenger.RemoveListener(GameEvent.HEALTH_UPDATED, OnHealthUpdated);
+        Messenger.RemoveListener(GameEvent.LEVEL_COMPLETE, OnLevelComplete);
+        Messenger.RemoveListener(GameEvent.LEVEL_FAILED, OnLevelFailed);
+        Messenger.RemoveListener(GameEvent.GAME_COMPLETE, OnGameComplete);
+    }
+
+    void OnLevelFailed()
+    {
+        StartCoroutine(FailLevel());
+    }
+
+    IEnumerator FailLevel()
+    {
+        levelEnding.gameObject.SetActive(true);
+        levelEnding.text = "Level Failed";
+
+        yield return new WaitForSeconds(2);
+
+        Managers.Player.Respawn();
+        Managers.Mission.RestartCurrent();
+    }
+
+    private void Start()
+    {
+        OnHealthUpdated();
+        levelEnding.gameObject.SetActive(false);
+        popup.gameObject.SetActive(false);
+    }
+
+    void OnLevelComplete()
+    {
+        StartCoroutine(CompleteLevel());
+    }
+    IEnumerator CompleteLevel()
+    {
+        levelEnding.gameObject.SetActive(true);
+        levelEnding.text = "Level Complete.";
+        yield return new WaitForSeconds(2);
+        Managers.Mission.GoToNext();
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            bool isShowing = popup.gameObject.activeSelf;
+            popup.gameObject.SetActive(!isShowing);
+            popup.Refresh();
+        }
+    }
+
+    void OnHealthUpdated()
+    {
+        string message = "Health: " + Managers.Player.health + "/" + Managers.Player.maxHealth;
+        healthLabel.text = message;
+    }
+
+}
