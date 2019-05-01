8358fd195a97d8e7e768b07cbf0d86ba0821cf7a
ITSupapitch <59070169@it.kmitl.ac.th>
2019-05-01 10:47:24 +0700

    Lab_wk15_updated1
    follow Labsheet page 1 - 5

---
+++ week15/Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour {
+	[SerializeField] private string itemName;
+
+	void OnTriggerEnter(Collider other) {
+		//		Debug.Log ("item collected = " + itemName);	
+		Managers.Inventory.AddItem(itemName);
+		Destroy (this.gameObject);
+	}
+}
