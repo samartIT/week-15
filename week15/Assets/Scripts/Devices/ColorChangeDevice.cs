26e9429e18a1945f1adedc2bcbc5325b70142978
france14331 <59070042@it.kmitl.ac.th>
2019-05-01 11:39:02 +0700

    GUI

---
+++ week15/Assets/Scripts/Devices/ColorChangeDevice.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeDevice : MonoBehaviour {
+	public void Operate() {
+		float R = Random.Range (0f, 1f);
+		float G = Random.Range (0f, 1f);
+		float B = Random.Range (0f, 1f);
+		Color random = new Color (R, G, B);
+		GetComponent<Renderer> ().material.color = random;
+	}
+}
+
