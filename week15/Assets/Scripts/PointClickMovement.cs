4f51673f5f345b6efde98384bedee538ff7b5041
Pawin <42163060+PawinThamroungthong@users.noreply.github.com>
2019-05-01 10:03:18 +0700

    modify PointClickMovement Pg.5

---
+++ week15/Assets/Scripts/PointClickMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PointClickMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float moveSpeed = 6.0f;
+    public float rotSpeed = 15.0f;
+    public float deceleration = 20f;
+    public float targetBuffer = 1.5f;
+
+    private CharacterController _charController;
+    private Animator _animaor;
+    private float _curSpeed = 0f;
+    private Vector3 _targetPos = Vector3.one;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+        _animaor = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit mouseHit;
+            if(Physics.Raycast(ray, out mouseHit))
+            {
+                _targetPos = mouseHit.point;
+                _curSpeed = moveSpeed;
+            }
+        }
+        if (_targetPos != Vector3.one)
+        {
+            Vector3 adjustedPos = new Vector3(_targetPos.x, transform.position.y, _targetPos.z);
+            Quaternion targetRot = Quaternion.LookRotation(adjustedPos - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotSpeed * Time.deltaTime);
+            movement = _curSpeed * Vector3.forward;
+            movement = transform.TransformDirection(movement);
+        }
+        if (Vector3.Distance(_targetPos, transform.position) < targetBuffer)
+        {
+            _curSpeed -= deceleration * Time.deltaTime;
+            if (_curSpeed <= 0)
+                _targetPos = Vector3.one;
+        }
+        _animaor.SetFloat("Speed", movement.sqrMagnitude);
+        movement *= Time.deltaTime;
+        _charController.Move(movement);
+    }
+}
