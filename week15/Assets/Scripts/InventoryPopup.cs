80bc5c43fe37c968e1e125811a383093bc2a0f29
Munawang <59070036@it.kmitl.ac.th>
2019-05-01 13:09:52 +0700

    Modify “InventoryPopup.cs”, a bit long.

--- week15/Assets/Scripts/InventoryPopup.cs
+++ week15/Assets/Scripts/InventoryPopup.cs
@@ -1,11 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class InventoryPopup : MonoBehaviour
 {
+    [SerializeField] private Image[] itemIcons;
+    [SerializeField] private Text[] itemLabels;
+
+    [SerializeField] private Text curItemLabel;
+    [SerializeField] private Button equipButton;
+    [SerializeField] private Button useButton;
+
+    private string _curItem;
+
     public void Refresh()
     {
+        List<string> itemList = Managers.Inventory.GetItemList();
+
+        int len = itemIcons.Length;
+        for (int i = 0; i < len; i++)
+        {
+            if(i < itemList.Count)
+            {
+                itemIcons[i].gameObject.SetActive(true);
+                itemLabels[i].gameObject.SetActive(true);
+
+                string item = itemList[i];
+
+                Sprite sprite = Resources.Load<Sprite>("Icons/" + item);
+                itemIcons[i].sprite = sprite;
+                itemIcons[i].SetNativeSize();
+
+                int count = Managers.Inventory.GetItemCount(item);
+                string message = "x" + count;
+                if (item == Managers.Inventory.equippedItem)
+                {
+                    message = "Equipped\n" + message;
+                }
+                itemLabels[i].text = message
+            }
+        }
 
     }
 }
