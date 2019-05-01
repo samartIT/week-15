06e87f82d2436a9c3fc23dc8ed86ae6539734c30
OKKachasom <59070001@kmitl.ac.th>
2019-05-01 11:35:29 +0700

    part I

---
+++ week15/Assets/Scripts/UIController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Text healthLabel;
+    [SerializeField] private InventoryPopup popup;
+
+    void Awake()
+    {
+        Messenger.AddListener(GameEvent.HEALTH_UPDATED, OnHealthUpdated);
+    }
+
+    void OnDestroy()
+    {
+        Messenger.RemoveListener(GameEvent.HEALTH_UPDATED, OnHealthUpdated);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnHealthUpdated();
+        popup.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            bool isShowing = popup.gameObject.activeSelf;
+            popup.gameObject.SetActive(!isShowing);
+            popup.Refresh();
+        }
+    }
+    
+    void OnHealthUpdated()
+    {
+        string message = "Health: " + Managers.Player.health + "/" + Managers.Player.maxHealth;
+        healthLabel.text = message;
+    }
+}
