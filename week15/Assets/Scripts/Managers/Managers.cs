94daea621c277cd6e78b8e42ff11620f4fad3f34
bankIT59 <59070079@it.kmitl.ac.th>
2019-05-04 11:27:14 +0700

    cc

--- week15/Assets/Scripts/Managers/Managers.cs
+++ week15/Assets/Scripts/Managers/Managers.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 [RequireComponent (typeof(PlayerManager))]
 [RequireComponent(typeof(InventoryManager))]
+[RequireComponent(typeof(MissionManager))] //new add
 
 public class Managers : MonoBehaviour {
 	public static PlayerManager Player {
@@ -16,15 +17,25 @@ public class Managers : MonoBehaviour {
 		private set;
 	}
 
+    public static MissionManager Mission { 
+        get;
+        private set;
+    }
+
 	private List<IGameManager> _startSequence;
 
 	void Awake(){
+        // make sure object will be passed to next scene
+        DontDestroyOnLoad(gameObject);
+
 		Player = GetComponent<PlayerManager>();
 		Inventory = GetComponent<InventoryManager> ();
+        Mission = GetComponent<MissionManager>(); 
 
 		_startSequence = new List<IGameManager> ();
 		_startSequence.Add (Player);
 		_startSequence.Add (Inventory);
+        _startSequence.Add(Mission);
 
 		StartCoroutine (StartupManagers());
 	}
@@ -51,11 +62,14 @@ public class Managers : MonoBehaviour {
 
 			if (numReady < lastReady)
 				Debug.Log ("Progress = " + numReady + "/" + numModules);
+            Messenger<int, int>.Broadcast(
+                StartupEvent.MANAGERS_PROGRESS, numReady, numModules);
 
 			yield return null;
 		}
 
 		Debug.Log ("All managers started");
+        Messenger.Broadcast(StartupEvent.MANAGERS_STARTED);
 	}
 
 	// Use this for initialization
