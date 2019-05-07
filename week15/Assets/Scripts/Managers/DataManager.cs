99b6f8d1fb46109d1275ff44d546da83794aff37
59070036 <59070036@it.kmitl.ac.th>
2019-05-07 21:02:47 +0700

    DataManager

---
+++ week15/Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class DataManager : MonoBehaviour, IGameManager {
+    public ManagerStatus status { get; private set; }
+
+    private string _filename;
+    private NetworkService _network;
+
+    public void Statup(NetworkService service)
+    {
+        Debug.Log("Data Manager starting ...");
+        _network = service;
+        _filename = Path.Combine(Application.persistentDataPath, "game.dat");
+        status = ManagerStatus.Started;
+    }
+
+    public void SaveGameState()
+    {
+        Dictionary<string, object> gamestate = new Dictionary<string, object>();
+        gamestate.Add("inventory", Managers.Inventory.GetData());
+        gamestate.Add("health", Managers.Player.health);
+        gamestate.Add("maxHealth", Managers.Player.maxHealth);
+        gamestate.Add("curLevel", Managers.Player.curLevel);
+        gamestate.Add("maxLevel", Managers.Player.maxLevel);
+        FileStream stream = File.Create(_filename);
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(stream, gamestate);
+        stream.Close();
+    }
+
+    public void LoadGameState
+
+}
