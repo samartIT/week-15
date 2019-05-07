c2f72fc1c90cdbadcbda351eeb83eddc563e08c5
59070036 <59070036@it.kmitl.ac.th>
2019-05-07 17:10:29 +0700

    Page9

--- week15/Assets/Scripts/Managers/Managers.cs
+++ week15/Assets/Scripts/Managers/Managers.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 [RequireComponent (typeof(PlayerManager))]
 [RequireComponent(typeof(InventoryManager))]
+[RequireComponent(typeof(MissionManager))]
 
 public class Managers : MonoBehaviour {
 	public static PlayerManager Player {
@@ -16,15 +17,25 @@ public class Managers : MonoBehaviour {
 		private set;
 	}
 
+    public static MissionManager Mission
+    {
+        get;
+        private set;
+    }
+
 	private List<IGameManager> _startSequence;
 
 	void Awake(){
+        DontDestroyOnLoad(gameObject);
+
 		Player = GetComponent<PlayerManager>();
 		Inventory = GetComponent<InventoryManager> ();
+        Mission = GetComponent<MissionManager>();
 
 		_startSequence = new List<IGameManager> ();
 		_startSequence.Add (Player);
 		_startSequence.Add (Inventory);
+        _startSequence.Add(Mission);
 
 		StartCoroutine (StartupManagers());
 	}
@@ -39,23 +50,28 @@ public class Managers : MonoBehaviour {
 		int numModules = _startSequence.Count;
 		int numReady = 0;
 
-		while (numReady < numModules){
-			int lastReady = numReady;
-			numReady = 0;
+        while (numReady < numModules)
+        {
+            int lastReady = numReady;
+            numReady = 0;
 
-			foreach(IGameManager manager in _startSequence){
-				if (manager.status == ManagerStatus.Started) {
-					numReady++;
-				}
-			}
+            foreach (IGameManager manager in _startSequence)
+            {
+                if (manager.status == ManagerStatus.Started)
+                {
+                    numReady++;
+                }
+            }
 
-			if (numReady < lastReady)
-				Debug.Log ("Progress = " + numReady + "/" + numModules);
-
+            if (numReady < lastReady) { 
+                Debug.Log("Progress = " + numReady + "/" + numModules);
+            Messenger<int, int>.Broadcast(
+                StartupEvent.MANAGERS_PROGRESS, numReady, numModules);
+            }
 			yield return null;
 		}
-
 		Debug.Log ("All managers started");
+        Messenger.Broadcast(StartupEvent.MANAGERS_STARTED);
 	}
 
 	// Use this for initialization
